d8890b40bdbac79184df9505cb4a9bab04f8fcf0
Lukasz Warzecha <luke.warzecha@gmail.com>
2020-02-17 00:20:37 +0000

    User model

---
+++ src/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Brillianttechnologies\GivelyModels;
+
+/**
+ * Description of User
+ *
+ * @author Lukasz
+ */
+class User
+{
+    const USER_STATUS_INACTIVE = 0;
+    const USER_STATUS_ACTIVE = 1;
+}
